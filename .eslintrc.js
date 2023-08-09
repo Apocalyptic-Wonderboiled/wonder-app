c8bfb6c3c294d848c1e7f6637d4a0681d46dbd36
sora_uzu <naruto07170717@gmail.com>
2023-08-09 22:02:27 +0900

    packageを追加、lint設定

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    ecmaFeatures: {
+      "jsx": true,
+    }
+  },
+  plugins: [
+    'react',
+  ],
+  "ignorePatterns": [
+    ".eslintrc.js"
+  ],
+  rules: {
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single'],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-use-before-define': "off",
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx'],
+      },
+    },
+  }
+}
