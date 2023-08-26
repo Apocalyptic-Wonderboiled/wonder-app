1d224bc2070f4fd52f1f9e828ec6d0c766fd0be1
komaitaira <komaitaira@gmail.com>
2023-08-26 13:16:41 +0900

    #4 ’○○’ is missing in props validationエラーの回避

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
     'no-use-before-define': "off",
+    'react/prop-types': 'off',
   },
   settings: {
     'import/resolver': {
