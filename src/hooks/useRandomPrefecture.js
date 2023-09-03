080578d765deadd17d3a6e798efde532364ee930
sora_uzu <naruto07170717@gmail.com>
2023-09-03 09:01:08 +0900

    useRandomPrefecture.js内のimport名変更

--- src/hooks/useRandomPrefecture.js
+++ src/hooks/useRandomPrefecture.js
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import prefecturesData from '../data/prefectures.json';
+import prefectures from '../data/prefectures.json';
 
 export const useRandomPrefecture = () => {
   const [prefecture, setPrefecture] = useState(null);
 
   const getPrefecture = () => {
-    const randomIndex = Math.floor(Math.random() * prefecturesData.length);
-    setPrefecture(prefecturesData[randomIndex]);
+    const randomIndex = Math.floor(Math.random() * prefectures.length);
+    setPrefecture(prefectures[randomIndex]);
   };
 
   return [prefecture, getPrefecture];
