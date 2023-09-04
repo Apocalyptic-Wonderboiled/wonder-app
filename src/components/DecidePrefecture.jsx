fc3aaafaff370ded4c11df911dc89a2b23cd653e
komaitaira <komaitaira@gmail.com>
2023-09-04 23:10:09 +0900

    #20 DecidePrefectureにボタンを移譲

--- src/components/DecidePrefecture.jsx
+++ src/components/DecidePrefecture.jsx
@@ -1,3 +1,25 @@
+import { useEffect } from 'react';
+import { useChatGpt } from './../hooks/useChatGpt';
+import { useRandomPrefecture } from './../hooks/useRandomPrefecture';
+import { StartStopButton } from './StartStopButton';
+
 export const DecidePrefecture = () => {
-  return <h1>都道府県画像シャッフル画面</h1>;
+  const [text, getReply] = useChatGpt();
+  const [prefecture, getPrefecture] = useRandomPrefecture();
+
+  const handleClick = () => getPrefecture();
+
+  useEffect(() => {
+    if (!prefecture) return;
+    getReply(prefecture.romaji);
+  }, [prefecture]);
+
+  return (
+    <>
+      <h1>都道府県画像シャッフル画面</h1>
+      <img src={'都道府県の画像のパス'} alt="都道府県の画像" />
+      <p>{text && text}</p>
+      <StartStopButton handleClick={handleClick} text="STOP" />
+    </>
+  );
 };
