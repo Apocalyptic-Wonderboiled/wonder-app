428c37316d25510006fc9a702578401c201ba1ad
komaitaira <komaitaira@gmail.com>
2023-09-15 00:13:50 +0900

    #38 backgroundを設定

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import Container from '@mui/material/Container';
 import { Top } from './components/Top';
 import { DecidePrefecture } from './components/DecidePrefecture';
 import { useScreen } from './hooks/useScreen';
+import bgimage from './bg.png';
 
 export const ScreenContext = createContext();
+
+const background = { backgroundImage: `url(${bgimage})` };
 
 function App() {
   const screen = useScreen();
 
   return (
     <ScreenContext.Provider value={screen}>
-      <Container justifycontent="center" alignitems="center" style={{ minHeight: '100vh' }}>
-        {screen.isTop ? <Top handleClick={screen.setScreenDecidePrefecture} /> : <DecidePrefecture />}
-      </Container>
+      <div style={background}>
+        <Container justifycontent="center" alignitems="center" style={{ minHeight: '100vh' }}>
+          {screen.isTop ? <Top handleClick={screen.setScreenDecidePrefecture} /> : <DecidePrefecture />}
+        </Container>
+      </div>
     </ScreenContext.Provider>
   );
 }
