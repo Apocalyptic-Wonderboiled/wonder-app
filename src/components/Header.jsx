b556d28002ee6c0577e587a49d57b4775b5e38ec
kanp7 <tatsuya715tk@gmail.com>
2023-09-16 14:45:34 +0900

    TOP画面にアニメーションを追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <>
       <div className={styles.header}>
-        <h1>Prefecture Picker</h1>
+        <h1 className={styles.appName}>Prefecture Picker</h1>
         <h2 className={styles.subtitle}>旅に出よう 〜行き先ランダム決定アプリ〜</h2>
       </div>
     </>
